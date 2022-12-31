e4cdf26c2305aaf967c2cf64bbc53c7084d5e190
GennaroAlphonse <genalfa@gmail.com>
2022-12-31 12:38:27 -0600

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports= {
  branches:"master",
   repositoryUrl: "https://github.com/alialaa/react-app",
-  plugins: [ '@semantic-release/commit-analyzer','@semantic-release/release-notes-generator','@semantic-release/npm','@semantic-release/github']
+    plugins: [ "@semantic-release/commit-analyzer","@semantic-release/release-notes-generator","@semantic-release/npm", ["@semantic-release/github",{assets:[{path: "build.zip", label: "Build"},{path: "coverage.zip", label: "Coverage"}] }]]
 }
   
