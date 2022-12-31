ab9dcb6f14dced0684a57c4db2f68674f9e66002
GennaroAlphonse <genalfa@gmail.com>
2022-12-31 12:18:19 -0600

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,6 @@
+module.exports= {
+ branches:"master",
+  repositoryUrl: "https://github.com/alialaa/react-app",
+  plugins: [ '@semantic-release/commit-analyzer','@semantic-release/release-notes-generator','@semantic-release/npm','@semantic-release/github']
+}
+  
