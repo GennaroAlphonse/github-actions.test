925b328fceea9080837aa6819b0c7638fa8d1a8e
GennaroAlphonse <genalfa@gmail.com>
2022-12-31 21:37:47 +0000

    Update index.js

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,15 +2,27 @@ const core= require('@actions/core');
 const github= require('@actions/github');
 
 try{
+  
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
   //throw (new Error("some message"));
   const name= core.getInput("who-to-greet");
+  
+  core.setSecret(name);
 
   console.log('Hello ${name}');
 
   const time= new Date();
   core.setOutput("time", time.toTimeString());
+  
+  core.startGroup('Logging Github object');
 
-  console.log(JSON.stringify(github,null,"/t"));}
+  console.log(JSON.stringify(github,null,"/t"));
+  core.endGroup();
+  
+  core.exportVariable("HELLO", "Hello");
+}
 catch (error){
   core.setFailed(error.message);
 }
