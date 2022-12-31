0add487230a5782cfdb5a83fc02055b2e0866d21
GennaroAlphonse <genalfa@gmail.com>
2022-12-31 22:24:49 +0000

    Create index.js

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,26 @@
+const core= required('@actions/core');
+const github= required('@actions/github');
+
+try{
+  const token=core.getInput('token');
+  const title=core.getInput('title');
+  const body=core.getInput('body');
+  const asignees=core.getInput('asignees');
+  
+  const octokit = new github.Github(token);
+  
+  const response=octokit.issues.create({
+    //owner= github.context.repo.owner,
+    //repo= github.context.repo.repo,
+    ... github.context.repo,
+    title,
+    body,
+    assignees: assignees ? assignees.split('\n') : undefined
+  });
+  
+  core.setOutput("issue",JSON.stringify(response.data));
+  
+  
+}catch(error){
+  core.setFailed(error.message);
+}
