f460ce47eadf4ef820fab7e98c43de4150df51f1
GennaroAlphonse <genalfa@gmail.com>
2023-01-02 17:38:17 +0000

    Update entrypoint.sh

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,1 +1,23 @@
+echo "::debug ::Debug message"
+echo "::warning ::Warning message"
+echo "::error ::Error message"
+
+echo "::add-mask::$1"
+
 echo "Hello $1"
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo "Some stuff"
+echo "Some stuff"
+echo "Some stuff"
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::hello"
+
+
+
+
+
